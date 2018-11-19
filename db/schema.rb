df93d74e34c985dd58af81b099de822167afc60a
Adline Isiji <adlyne@gmail.com>
2018-11-19 16:11:44 +0100

    AddChatToMessages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_125352) do
+ActiveRecord::Schema.define(version: 2018_11_19_150720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_11_19_125352) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "chat_id"
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -40,5 +42,6 @@ ActiveRecord::Schema.define(version: 2018_11_19_125352) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
 end
