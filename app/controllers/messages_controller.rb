79709ccbe721d3dc80d8413dba842188eb233293
Adline Isiji <adlyne@gmail.com>
2018-11-20 12:58:50 +0100

    Create chatschannel & script in the show html

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,23 @@
 class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
-    if @message.save # false
-      redirect_to @message
+    @chat = Chat.find(params[:chat_id])
+    @message.chat = @chat
+    @message.user = current_user
+    if @message.save
+      ActionCable.server.broadcast("chat_#{@chat.id}", {
+        message_partial: render(partial: "messages/message", locals: {message: @message})
+        })
+
     else
-      render :new
+      respond_to do |format|
+        format.html {
+          render "chats/show"
+        }
+        format.js
+      end
     end
+
   end
 
   private
