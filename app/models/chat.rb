25a98bb62943f1b1316fb003576a30c15702896f
Janina Kokkonen <janina.kokkonen@gmail.com>
2018-11-23 13:26:23 +0100

    login path to chat works

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,5 @@
 class Chat < ApplicationRecord
   has_many :messages
-  belongs_to :helpee, class_name: "User"
+  belongs_to :helpee, class_name: "User", optional: true
   belongs_to :helper, class_name: "User", optional: true
 end
