7c0e63f88701ca878c4c3c19142dbba765879126
Ana Ivan <ana.ivan.m@gmail.com>
2018-11-25 18:41:04 +0100

    Set meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,7 @@ class ApplicationController < ActionController::Base
     # request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
 
+def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+end
 end
