c2f7de17197ee1ecbcfdffff99e5ed02faee8000
irene jona smith irenejonasmith12@gmail.com <irenesmith@gmx.com>
2018-11-26 13:38:59 +0100

    list of open chats added to volunteer dashboard, with assignment on click

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,4 +3,12 @@ class DashboardsController < ApplicationController
     @user = current_user
     @open_chats = Chat.where(helper: nil).where.not(helpee: nil)
   end
+
+  def get_open_chats
+    # render partial: 'open_chats_list', locals: {chats: @open_chats}
+    @open_chats = Chat.where(helper: nil).where.not(helpee: nil)
+    respond_to do |format|
+      format.js  # <-- idem
+    end
+  end
 end
