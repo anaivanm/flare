f21c3c121ba3369d45842000e5ccafba90d26f46
irene jona smith irenejonasmith12@gmail.com <irenesmith@gmx.com>
2018-11-28 14:20:55 +0100

    something

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import "../modal.js";
 import "./map.js";
 import "./dashboard.js";
+import "./refresh_chat_banner.js";
 
 import { visibleOnScroll } from "../components/navbar.js"
 
